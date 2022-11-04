55af4181ebcfb7b8293c2ba773bdbef663c7ef95
EIDiamond <105827519+EIDiamond@usersnoreply.github.com>
2022-11-05 02:11:57 +0300

    Init commit some files

---
+++ README.md
@@ -0,0 +1,29 @@
+## Description
+
+## Features
+
+## Before Start
+### Dependencies
+
+### Required configuration (minimal)
+
+### Run
+Recommendation is to use python 3.10. 
+
+Run main.py
+
+## Configuration
+Configuration can be specified via [settings.ini](settings.ini) file.
+
+## CSV files (default storage)
+
+## Use case
+
+### Example
+
+## Logging
+All logs are written in logs/collector.log.
+Any kind of settings can be changed in main.py code
+
+## Disclaimer
+The author is not responsible for any errors or omissions, or for the trade results obtained from the use of this tool. 
---
+++ main.py
@@ -0,0 +1,37 @@
+import asyncio
+import logging
+import os
+import sys
+
+from logging.handlers import RotatingFileHandler
+
+# the configuration file name
+CONFIG_FILE = "settings.ini"
+
+logger = logging.getLogger(__name__)
+
+
+def prepare_logs():
+    if not os.path.exists("logs/"):
+        os.makedirs("logs/")
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
+        handlers=[RotatingFileHandler('logs/collector.log', maxBytes=100000000, backupCount=10, encoding='utf-8')],
+        encoding="utf-8"
+    )
+
+
+if __name__ == '__main__':
+    prepare_logs()
+
+    logger.info("Data collector has been started")
+
+    try:
+        pass
+
+    except Exception as ex:
+        logger.error(f"Error has been occurred: {repr(ex)}")
+
+    logger.info("Data collector has been finished.")
